1f113b63199d7a859ccb9559604444fc8a7c9a2f
Tiago Angelo <tiago.angelo@sensedia.com>
2018-09-02 12:16:56 -0300

    Updating routes

--- src/main/kotlin/com/sensedia/creditcard/infra/RouteConfiguration.kt
+++ src/main/kotlin/com/sensedia/creditcard/infra/RouteConfiguration.kt
@@ -5,24 +5,23 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.function.server.HandlerFunction
-import org.springframework.web.reactive.function.server.RequestPredicates.*
-import org.springframework.web.reactive.function.server.RouterFunction
+import org.springframework.web.reactive.function.server.RequestPredicates.GET
+import org.springframework.web.reactive.function.server.RequestPredicates.POST
 import org.springframework.web.reactive.function.server.RouterFunctions.route
-import org.springframework.web.reactive.function.server.ServerResponse
 
 @Configuration
 @ComponentScan(basePackageClasses = [CreditCardHandler::class])
 class RouteConfiguration {
 
     @Bean
-    fun routes(handler: CreditCardHandler): RouterFunction<ServerResponse> {
-        return route(GET("/credit-card"), HandlerFunction<ServerResponse> { handler.all(it) }).andNest(path("/"),
-                route(
-                        POST("/"),
-                        HandlerFunction<ServerResponse> { handler.create(it) }))
-                .andRoute(
-                        GET("/{creditCardId}"),
-                        HandlerFunction<ServerResponse> { handler.findById(it) })
-    }
+    fun routes(handler: CreditCardHandler) = route(
+                    GET("/api/v1/credit-card"),
+                    HandlerFunction { handler.all(it) })
+            .andRoute(
+                    POST("/api/v1/credit-card"),
+                    HandlerFunction { handler.create(it) })
+            .andRoute(
+                    GET("/api/v1/credit-card/{creditCardId}"),
+                    HandlerFunction { handler.findById(it) })
 
 }
